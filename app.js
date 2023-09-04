f586b35972810e571ba704e37b934f96cf260f88
Eduardo <eduleon78@gmail.com>
2023-09-03 20:28:05 -0400

    fix

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cons = require('consolidate');
 
 let app = express();
 
